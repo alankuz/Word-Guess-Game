668d3e2adda5e90e88230a9e24ae361cbdc471e5
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-07-29 23:15:06 -0600

    bug fix: made wins and losses better. Upper and lower case both work, cannot enter key other than letter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,12 +23,14 @@ var stand = document.getElementById("standing");
 var targetDiv = document.getElementById("lettersGuessed");
 var guessDiv = document.getElementById("guesses");
 var ansDiv = document.getElementById("theWord");
+var roundNum = document.getElementById("rounds");
 var word = words[Math.floor(Math.random() * words.length)];
 var answerArray = [];
 wordTwo = word.split('');
 var remainingLetters = word.length;
 var guessArray = [];
 var guessesLeft = 6;
+var gameOver = false;
 
 
 // Array creator -------------------------------------------------
@@ -44,7 +46,7 @@ answerLog = function (ke) {
   targetDiv.textContent = guessArray.join(' ');
 
   for (var i = 0; i < word.length; i++) {
-// checks if keyentry fits the word loaded--------
+    // checks if keyentry fits the word loaded--------
     if (ke === word[i]) {
       console.log("if");
       isLetter = true;
@@ -53,13 +55,14 @@ answerLog = function (ke) {
       ansDiv.textContent = answerArray.join(' ');
       console.log(answerArray);
       console.log("winner------ " + answerArray.toString() + "----" + wordTwo.toString())
-// If all charactors match with the full word then this will give you a win-----
+      // If all charactors match with the full word then this will give you a win-----
       if (wordTwo.toString() === answerArray.toString()) {
         again.style.display = "block";
         wins++;
         win.textContent = 'Wins: ' + wins;
         stand.textContent = 'You WIN!!';
         stand.style.display = "block";
+        gameOver = true;
       }
     }
   }
@@ -75,10 +78,22 @@ answerLog = function (ke) {
 }
 // key entry function, allows the keys to be read and also checks if you have used all guesses------
 document.onkeyup = function (keyEntry) {
-  var ke = keyEntry.key;
-  keyCheck = guessArray.indexOf(ke);
-  if (keyCheck < 0) {
-    answerLog(ke);
+  if (keyEntry.keyCode >= 65 && keyEntry.keyCode <= 90) {
+    if (gameOver === false) {
+      var keFix = keyEntry.key;
+      var ke = keFix.toLocaleLowerCase();
+      console.log(keyEntry.keyCode)
+      keyCheck = guessArray.indexOf(ke);
+      if (keyCheck < 0) {
+        answerLog(ke);
+      }
+    } else {
+      alert('To play again press "Next Round"')
+    }
+
+  }
+  else {
+    alert('Not Valid Entry. Letters only.')
   }
 }
 // Tells you how many guesses you have left before you lose. 
@@ -88,17 +103,20 @@ guessesDone = function () {
   guessDiv.textContent = guessesLeft;
   if (guessesLeft < 1) {
     again.style.display = "block";
-    round++
     win.textContent = 'Wins: ' + wins;
     stand.textContent = 'You Lose...';
     stand.style.display = "block";
+    gameOver = true;
+
   }
 }
 // Game reset function, used to get back to square one and to set up new rounds win||lose-------
 var reset = function () {
+  gameOver = false;
   again.style.display = "none";
   stand.style.display = "none";
   guessesLeft = 6;
+  roundNum.textContent = 'Rounds: ' + rounds;
   guessDiv.textContent = guessesLeft;
   guessArray = [];
   answerArray = [];
